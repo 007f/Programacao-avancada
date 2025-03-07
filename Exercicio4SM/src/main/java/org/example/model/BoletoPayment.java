6bc4628fa31a6b702b2c0b7dc213d49c7afa614c
007f <88677725+007f@users.noreply.github.com>
2025-03-06 22:27:46 -0300

    Update BoletoPayment.java

--- Exercicio4SM/src/main/java/org/example/model/BoletoPayment.java
+++ Exercicio4SM/src/main/java/org/example/model/BoletoPayment.java
@@ -3,6 +3,7 @@ package org.example.model;
 public class BoletoPayment implements PaymentStrategy {
     @Override
     public void processPayment(double amount) {
-
+        System.out.println("Boleto de indentificação: 123456782345 Criando com sucesso");
+        System.out.println("Valor: " + amount);
     }
 }
