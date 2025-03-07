80945e85fbf2310850f46ba241585bd2459b2463
007f <88677725+007f@users.noreply.github.com>
2025-03-06 22:16:53 -0300

    Update PixPayment.java

--- Exercicio4SM/src/main/java/org/example/model/PixPayment.java
+++ Exercicio4SM/src/main/java/org/example/model/PixPayment.java
@@ -3,6 +3,7 @@ package org.example.model;
 public class PixPayment implements PaymentStrategy {
     @Override
     public void processPayment(double amount) {
-
+        System.out.println("Chave pix: 007felipe.mello@gmail.com");
+        System.out.println("Valor: " + amount);
     }
 }
