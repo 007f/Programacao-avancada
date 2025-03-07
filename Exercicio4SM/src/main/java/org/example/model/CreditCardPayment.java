e11ef485de2c3983f3302f6e12957daea40285f9
007f <88677725+007f@users.noreply.github.com>
2025-03-07 11:28:58 -0300

    Update CreditCardPayment.java

--- Exercicio4SM/src/main/java/org/example/model/CreditCardPayment.java
+++ Exercicio4SM/src/main/java/org/example/model/CreditCardPayment.java
@@ -3,6 +3,9 @@ package org.example.model;
 public class CreditCardPayment implements PaymentStrategy {
     @Override
     public void processPayment(double amount) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o numero do cartão: ");
+        String cardNumber = scanner.nextLine();
+        System.out.println("Valor: " + amount);
     }
 }
